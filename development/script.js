8b9738193938355cb16c4b729ba009158d549737
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2022-09-20 15:29:18 -0500

    empty email input after submission

--- development/script.js
+++ development/script.js
@@ -1,8 +1,10 @@
 const popup = document.getElementById('pop-up');
 const form = document.getElementById('form');
+const emailInput = document.getElementById('email');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  emailInput.value = '';
 
   popup.classList.toggle('pop-up--show');
   setTimeout(() => {
